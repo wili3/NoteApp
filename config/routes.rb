61451005d75f4b096710396826ee5ccd7a3ad60d
Guillem <guillem.sannicolas@keradgames.com>
2017-05-22 21:56:29 +0200

    course

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
-  root 'welcome#index'
-
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
+
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
